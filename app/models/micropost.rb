a0063167b00c9415659764e471a460c63c8a5bb1
haobang <b_hao@timedia.co.jp>
2018-04-09 19:04:07 +0900

    static_page

--- app/models/micropost.rb
+++
@@ -1,6 +0,0 @@
-class Micropost < ApplicationRecord
-	#validtes :content, length: {maximum: 140}
-	belongs_to :user
-	validates :content, length: { maximum: 140 },
-						presence: true
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
+  get 'foo/bar'
+
+  get 'foo/bz'
+
   get 'static_pages/home'
 
   get 'static_pages/help'
 
-  resources :microposts
-  resources :users
+  get 'static_pages/about'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   #root 'application#hello'
   #root 'users#index'
-  root 'microposts#index'
+  root 'static_pages#home'
 end
